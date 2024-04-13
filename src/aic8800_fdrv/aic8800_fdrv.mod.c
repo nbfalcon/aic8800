d9dff30ea6815aa4f13da8f7e2f07118bb5fc97d
Nikita Bloshchanevich <nikblos@outlook.com>
2024-04-13 19:49:09 +0200

    Initial commit (with SGI patch)

---
+++ src/aic8800_fdrv/aic8800_fdrv.mod.c
@@ -0,0 +1,46 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/elfnote-lto.h>
+#include <linux/export-internal.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+#ifdef CONFIG_UNWINDER_ORC
+#include <asm/orc_header.h>
+ORC_HEADER;
+#endif
+
+BUILD_SALT;
+BUILD_LTO_INFO;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+
+MODULE_INFO(depends, "cfg80211,aic_load_fw");
+
+MODULE_ALIAS("usb:vA69Cp8801d*dc*dsc*dp*icFFiscFFipFFin*");
+MODULE_ALIAS("usb:vA69Cp8D81d*dc*dsc*dp*icFFiscFFipFFin*");
+MODULE_ALIAS("usb:vA69Cp88DCd*dc*dsc*dp*icFFiscFFipFFin*");
+MODULE_ALIAS("usb:vA69Cp88DDd*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v368Bp88DFd*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v2604p0013d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v2604p0014d*dc*dsc*dp*ic*isc*ip*in*");
+
+MODULE_INFO(srcversion, "F151D71D04FCA385D7FE2C8");
+MODULE_INFO(rhelversion, "9.99");
---
+++ src/aic_load_fw/aicwf_txq_prealloc.h
@@ -0,0 +1,4 @@
+void *aicwf_prealloc_txq_alloc(size_t size);
+
+void aicwf_prealloc_txq_free(void);
+
